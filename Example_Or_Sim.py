ab5fb8ddc020be97850eea9a206deca79aa74ccb
thoulden <101445737+thoulden@users.noreply.github.com>
2024-12-02 18:38:11 +0000

    Update Example_Or_Sim.py

--- Example_Or_Sim.py
+++ Example_Or_Sim.py
@@ -21,7 +21,7 @@ if params.get('page') == ['math_appendix']:
     \dot{S}_t &=& \bar{R}^{\lambda \alpha} {C}_t^{\lambda(1-\alpha)} S_t^{1-\beta(S_t)}
     """)
     st.markdown(r"""
-    where $S$ is the software level, $\bar{R}$ is stock of human AI researchers at the time GPT-6 is released (we assume this is a fixed quantity over time), $C$ is the compute available for research on AI, and $\alpha \in (0,1)$ dictates the contributions of researchers vs. compute to progress in AI. Importantly, the degree of diminishing returns, $\beta$ is a function of $S$. We assume the functional form for $\beta$ is such that everytime the software level closes half the gap between it's level and some software cieling, $S_{\text{ceiling}}$, $\beta$ doubles. This functional form is given by   
+    where $S$ is the software level, $\bar{R}$ is stock of human AI researchers at the time GPT-6 is released (we assume this is a fixed quantity over time), $C$ is the compute available for research on AI, $\lambda$ is the degree of parrallelizability in research, and $\alpha \in (0,1)$ dictates the contributions of researchers vs. compute to progress in AI. Importantly, the degree of diminishing returns, $\beta$ is a function of $S$. We assume the functional form for $\beta$ is such that everytime the software level closes half the gap between it's level and some software cieling, $S_{\text{ceiling}}$, $\beta$ doubles. This functional form is given by   
     """)
     st.latex(r"""
     \beta(S_t) &=& \beta_0 \bigg(1- \frac{\frac{S}{\bar{S}} - 1}{\frac{S_{\text{ceiling}}}{\bar{S}} - 1}\bigg)^{-1}
@@ -34,11 +34,31 @@ if params.get('page') == ['math_appendix']:
      st.latex(r"""
     g_{S,0} &=& \bar{R}^{\lambda \alpha} {C}_0^{\lambda(1-\alpha)} S_0^{-\beta(S_0) \implies S_0 = [2.77 \times (\bar{R}^{\lambda \alpha} {C}_0^{\lambda(1-\alpha)})^{-1}]^{\frac{-1}{\beta_0}}
     """)
-    
-    st.latex(r"""
-    \upsilon = \bar{R} \times \left(f^{\frac{1}{\lambda}} - 1\right) \times \left[2.77 \times \left(\bar{R}^\alpha C_0^{1-\alpha}\right)^{-\lambda}\right]^{\frac{1}{\beta_0}}
+    st.markdown(r"""
+    Next, we construct a case such that progress in AI would remain exponential (if it weren't for increasing diminishing returns to research); this case requires growth in compute. Specifically, given the form for the growth rate of $S$, for $g_S$ to remain constant we require
     """)
-    
+    st.latex(r"""
+    g_C = \frac{\beta_0}{\lambda (1-\alpha) g_{S_0}}
+    """)
+    st.markdown(r"""
+    Equations thus far are sufficient to simulate the path of $S$ over time. Simulation, on can see that this path looks generally exponential for some time until we get quite close to the software ceiling, in which case progress trails off. 
+
+    Now I turn to the case where we allow deployment of AI to accelerate AI research. Specifically, I assume that research accelerates by a factor $f$ after deployment of AI to research. To fix intuition, we can think of this as an increasing of the stock of researchers by $f^{\lambda \alpha}$ at $t=0$.
+
+    Next, we need to determine how AI progress contrinutes to the increased stock of AI researchers. I assume 
+    """)
+    st.latex(r"""
+    R_t = \bar{R} + \upsilon S_t
+    """)
+    st.markdown(r"""
+    where $\upsilon$ scales software into human resarcher equivalents. To calibrate $\uspilon$ we again look to the initial conditions of the model. Namely, so that the stock of available researchers after reaching GPT-6 is $f^{\lambda \alpha} \times \bar{R}$ we require
+    """)
+    st.latex(r"""
+    f^{\lambda \alpha} \bar{R} = \bar{R} + \upsilon S_0 \implies \upsilon = \bar{R} \times \left(f^{\frac{1}{\lambda}} - 1\right) \times \left[2.77 \times \left(\bar{R}^\alpha C_0^{1-\alpha}\right)^{-\lambda}\right]^{\frac{1}{\beta_0}}
+    """)
+    st.markdown(r"""
+    We will assume that growth in compute follows the same path as the base case where AI is not deployed to research
+    """)
     # Include a link to go back to the main page
     st.markdown("[Go back](?page=main)")
     
