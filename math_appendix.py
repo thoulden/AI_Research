3153fd09147a818d343c9a3436c464482dcca4b6
thoulden <101445737+thoulden@users.noreply.github.com>
2024-12-04 19:19:29 +0000

    Update math_appendix.py

--- math_appendix.py
+++ math_appendix.py
@@ -58,8 +58,17 @@ def display():
     \log(\beta_0) = \text{Intercept} + \text{Slope} \times \log(f) + \epsilon \quad \epsilon \sim N(0,\sigma^2)
     """)
     st.markdown(r"""
-    Ignoring $\epsilon$ for a second, to calibrate the slope we just need to ensure that choices of $f$ are scaled so that each choice in the range of possible $f$ can 'pick out' a possible $\beta_0$ value. To do this, set $\text{Slope} = \frac{\log(\beta_{0,max})-\log(\beta_{0,min})}{\log(\f{max}) -\log(\f{max})}$. Then we just have to solve for intercept that ensures (still, ignoring $\epsilon$) that picking $f_{min}$ will result in $\beta_{0,min}$ being chosen (and likewise for max values). Turns out the solution is $\text{Intercept} = \log(\beta_{0,min} - \text{Slope} \times \log(f_{min})$.
+    Ignoring $\epsilon$ for a second, to calibrate the slope we just need to ensure that choices of $f$ are scaled so that each choice in the range of possible $f$ can 'pick out' a possible $\beta_0$ value. To do this, set 
     """)
+    st.latex(r"""
+    \text{Slope} = \frac{\log(\beta_{0,max})-\log(\beta_{0,min})}{\log(\f{max}) -\log(\f{max})}
+    """)
+    st.markdown(r"""Then we just have to solve for intercept that ensures (still, ignoring $\epsilon$) that picking $f_{min}$ will result in $\beta_{0,min}$ being chosen (and likewise for max values). This yields
+    """)
+    st.latex(r"""
+    \text{Intercept} = \log(\beta_{0,min} - \text{Slope} \times \log(f_{min})
+    """)
+    
 
     # Include a link to go back to the main page
     st.markdown("[Go back](?page=main)")
