e6643f764db50d77bed327903b3acb94f8c79fb3
thoulden <101445737+thoulden@users.noreply.github.com>
2024-12-04 08:04:49 +0000

    Update single_simulation.py

--- single_simulation.py
+++ single_simulation.py
@@ -8,7 +8,7 @@ def run():
     run_simulation = st.sidebar.button('Run Simulation')
     
     # Simulation settings
-    delta_t = st.sidebar.number_input('Time step in years (delta_t)', min_value=0.0001, max_value=1.0, value=0.01, step=0.0001)
+    delta_t = st.sidebar.number_input('Time step in years', min_value=0.0001, max_value=1.0, value=0.01, step=0.0001)
     T = st.sidebar.number_input('Total simulation time in years (T)', min_value=0.1, max_value=20.0, value=8.0, step=0.1)
     time = np.arange(0, T, delta_t)
     num_steps = len(time)
