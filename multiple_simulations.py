7b112900fbe8510c1082c1c9b6159d4443b28c1c
thoulden <101445737+thoulden@users.noreply.github.com>
2024-12-04 15:08:20 +0000

    Update multiple_simulations.py

--- multiple_simulations.py
+++ multiple_simulations.py
@@ -251,7 +251,7 @@ def run():
 
         if enable_correlation:
             # Optional: Scatter plot to visualize correlation on linear scales
-            st.markdown("### Correlation between Beta_0 and f")
+            st.markdown(r"### Correlation between $\beta_0$ and f")
             fig_scatter, ax_scatter = plt.subplots(figsize=(10, 6))
             ax_scatter.scatter(f_samples, beta_0_samples, alpha=0.5, edgecolor='k', linewidth=0.5)
             ax_scatter.set_xlabel('f')
