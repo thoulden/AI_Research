0a2d239047aa89f0a3970471ada83aa164ec0264
thoulden <101445737+thoulden@users.noreply.github.com>
2024-12-04 14:48:33 +0000

    Update multiple_simulations.py

--- multiple_simulations.py
+++ multiple_simulations.py
@@ -250,11 +250,11 @@ def run():
         st.pyplot(fig_hist)
 
         # Plots correlation between f and beta
-        st.markdown("### Correlation between Beta_0 and f")
+        st.markdown("##### Correlation between Beta_0 and f")
         fig_scatter, ax_scatter = plt.subplots(figsize=(10, 6))
-        ax_scatter.scatter(np.log(f_samples), np.log(beta_0_samples), alpha=0.5, edgecolor='k', linewidth=0.5)
+        ax_scatter.scatter(np.log(f_samples), np.beta_0_samples, alpha=0.5, edgecolor='k', linewidth=0.5)
         ax_scatter.set_xlabel('Log(f)')
-        ax_scatter.set_ylabel('Log(Beta_0)')
+        ax_scatter.set_ylabel('Beta_0')
         ax_scatter.set_title('Scatter Plot of Log(Beta_0) vs. Log(f)')
         ax_scatter.grid(True)
         st.pyplot(fig_scatter)
